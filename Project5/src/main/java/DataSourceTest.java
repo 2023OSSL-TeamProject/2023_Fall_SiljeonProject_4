480a93918a0116fedab50e31729b7e0da277d78f
hansanghwa <hsh21900783@handong.ac.kr>
2023-12-12 20:35:22 +0900

    231212 상화 : board 파트 관리

---
+++ Project5/src/main/java/DataSourceTest.java
@@ -0,0 +1,22 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@ContextConfiguration(locations = "classpath:dispatcher-servlet.xml")
+public class DataSourceTest {
+    @Autowired
+    private DataSource ds;
+
+//    @Test
+    public void testConnection(){
+        try{
+            Connection con = ds.getConnection();
+            System.out.println("DB연결");
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
